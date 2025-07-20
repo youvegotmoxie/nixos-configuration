fb71c44c90bfe7fd57ea5e241ef3accc7e7f7200
MikeB <youvegotmoxie@gmail.com>
2025-07-19 23:58:04 -0800

    update

--- flake.nix
+++ flake.nix
@@ -3,18 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ...}:
     let
       lib = nixpkgs.lib;
     in {
-    nixosConfigurations = {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
       snafu-nixos = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
            # Import the main module
           ./configuration.nix
+          inputs.home-manager.nixosModules.default
         ];
       };
     };
