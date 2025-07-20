86097323516ae895c658942452e50a2f63190244
MikeB <youvegotmoxie@gmail.com>
2025-07-19 23:27:50 -0800

    update

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ...}: 
+  outputs = { self, nixpkgs, home-manager, ...}:
     let
       lib = nixpkgs.lib;
     in {
@@ -15,6 +17,12 @@
         modules = [
            # Import the main module
           ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mike = "./users/mike.nix"
+          }
         ];
       };
     };
