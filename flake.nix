56d039458ea09dfe6a007b9bc3dfb3bfeff9ebbf
MikeB <youvegotmoxie@gmail.com>
2025-07-20 00:08:14 -0800

    update

--- flake.nix
+++ flake.nix
@@ -10,20 +10,16 @@
     };
   };
 
-  outputs = { self, nixpkgs, ...}@inputs:
+  outputs = { self, nixpkgs, home-manager, ...}:
     let
       lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
     in {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+    nixosConfigurations = {
       snafu-nixos = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
            # Import the main module
           ./configuration.nix
-          inputs.home-manager.nixosModules.default
         ];
       };
     };
