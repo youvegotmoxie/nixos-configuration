37e8fcb3db7e13c9120acb46c4ff69089613b685
MikeB <youvegotmoxie@gmail.com>
2025-07-19 23:35:19 -0800

    update

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.mike = ./home.nix;
+            home-manager.users.mike = "./users/home-manager-mike.nix";
           }
         ];
       };
