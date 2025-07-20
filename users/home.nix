28b2c6763ee09290f6f0b1d9d6e401bcd2f286bb
MikeB <youvegotmoxie@gmail.com>
2025-07-20 10:23:40 -0800

    update

--- users/home.nix
+++ users/home.nix
@@ -75,6 +75,9 @@
       filter = {
         lfs = {
           clean = "git-lfs clean -- %f";
+          smudge = "git-lfs smudge -- %f";
+          process = "git-lfs filter-process";
+          required = true;
         };
       };
     };
