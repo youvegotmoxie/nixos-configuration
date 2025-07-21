4012045db735b9bb2b4a14d0d42a83bb327059a1
MikeB <youvegotmoxie@gmail.com>
2025-07-21 12:23:22 -0800

    updates

--- users/bash.nix
+++ users/bash.nix
@@ -10,45 +10,45 @@
     enableCompletion = true;
     enableVteIntegration = true;
     sessionVariables = {
-      GH_TOKEN = "${sops.secrets.gh_token}"
-        };
-      shellAliases = {
-        # Aliases not set by Atuin
-        ll = "eza -lahg --git-repos-no-status --git";
-        ls = "eza";
-        cat = "bat --paging=never --style=plain";
-      };
-      bashrcExtra = ''
-        if [[ $- != *i* ]] ; then
-                # Shell is non-interactive.  Be done now!
-                return
-        fi
-
-        # Enable VIM keybinds
-        set -o vi
-
-        # If a Python venv exists then activate it
-        if [ -d $HOME/.venv ]; then
-            source ~/.venv/bin/activate
-        fi
-
-        # Add SSH key to ssh-agent
-        eval "$(ssh-agent -s)"
-        ssh-add /home/mike/.ssh/id_rsa
-
-        # Helper function for git add+commit+push
-        git-sendit () {
-          local branch="$(git branch --show-current)"
-          git add .
-          if [ ! -z "$1" ]
-          then
-            local msg="trivial"
-          else
-            git commit
-          fi
-          git push origin "$branch"
-        }
-      '';
+      GH_TOKEN = "${sops.secrets.gh_token}";
     };
+    shellAliases = {
+      # Aliases not set by Atuin
+      ll = "eza -lahg --git-repos-no-status --git";
+      ls = "eza";
+      cat = "bat --paging=never --style=plain";
+    };
+    bashrcExtra = ''
+      if [[ $- != *i* ]] ; then
+              # Shell is non-interactive.  Be done now!
+              return
+      fi
 
-  }
+      # Enable VIM keybinds
+      set -o vi
+
+      # If a Python venv exists then activate it
+      if [ -d $HOME/.venv ]; then
+          source ~/.venv/bin/activate
+      fi
+
+      # Add SSH key to ssh-agent
+      eval "$(ssh-agent -s)"
+      ssh-add /home/mike/.ssh/id_rsa
+
+      # Helper function for git add+commit+push
+      git-sendit () {
+        local branch="$(git branch --show-current)"
+        git add .
+        if [ ! -z "$1" ]
+        then
+          local msg="trivial"
+        else
+          git commit
+        fi
+        git push origin "$branch"
+      }
+    '';
+  };
+
+}
