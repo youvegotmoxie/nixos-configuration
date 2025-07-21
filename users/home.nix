8478301e8c7889028581257b11d70da923a13b65
MikeB <youvegotmoxie@gmail.com>
2025-07-20 17:04:05 -0800

    updates

--- users/home.nix
+++ users/home.nix
@@ -21,8 +21,43 @@
   programs.home-manager.enable = true;
   services.home-manager.autoExpire.enable = true;
 
+  # Do this instead of setting up Chezmoi
   home.file.".pre-commit.yaml".text = ''
-    this is a test
+    # See https://pre-commit.com for more information
+    # See https://pre-commit.com/hooks.html for more hooks
+    repos:
+    - repo: https://github.com/pre-commit/pre-commit-hooks
+      rev: v5.0.0
+      hooks:
+      - id: trailing-whitespace
+      - id: check-added-large-files
+      - id: end-of-file-fixer
+    - repo: https://github.com/hhatto/autopep8
+      rev: 'v2.3.2'
+      hooks:
+      - id: autopep8
+    - repo: https://github.com/gitleaks/gitleaks.git
+      rev: 'v8.26.0'
+      hooks:
+      - id: gitleaks
+    - repo: https://github.com/koalaman/shellcheck-precommit
+      rev: v0.10.0
+      hooks:
+      - id: shellcheck
+        exclude: .*jenkins-slave$
+    - repo: https://github.com/hadolint/hadolint
+      rev: v2.13.1-beta
+      hooks:
+      - id: hadolint-docker
+        args:
+        - --ignore=DL3015 # Ignore not using --no-install-recommends with apt
+        - --ignore=DL3008 # Ignore not pinning all software package versions (apt-get)
+        - --ignore=DL3018 # Ignore not pinning all software package versions (apk)
+        - --ignore=SC1091 # Ignore missing shellcheck mock files
+    - repo: https://github.com/gruntwork-io/pre-commit
+      rev: 'v0.1.29'
+      hooks:
+      - id: terraform-validate
   '';
 
 
