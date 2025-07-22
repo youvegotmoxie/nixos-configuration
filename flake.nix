532aa9236cdec5cb92a890efb0c6e079d8b76295
MikeB <youvegotmoxie@gmail.com>
2025-07-22 07:47:50 -0800

    [nixos-ansible]

--- flake.nix
+++ flake.nix
@@ -16,41 +16,41 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-flatpak, sops-nix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nix-flatpak, sops-nix, ...}@inputs:
     let
-      system = "x86_64-linux";
+     system = "x86_64-linux";
     in
     {
-      nixosConfigurations = {
-        snafu-nixos = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            # Import the main module
-            ./configuration.nix
-            nix-flatpak.nixosModules.nix-flatpak
-            sops-nix.nixosModules.sops
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              # Move conflicting files out of the way instead of crashing home-manager
-              home-manager.backupFileExtension = "hmback";
-              home-manager.extraSpecialArgs.flake-inputs = inputs;
-              home-manager.users.mike.home.stateVersion = "25.05";
-              # Use Home Manager to manage Flatpaks
-              home-manager.users."mike".imports = [
-                # Add sops-nix support for home-manager
-                sops-nix.homeManagerModules.sops
-                # Flatpak NixOS configuration
-                nix-flatpak.homeManagerModules.nix-flatpak
-                ./users/mike/software/flatpak.nix
-                # Main home-manager configuration
-                ./users/mike/home.nix
-              ];
-            }
-          ];
-          specialArgs = { inherit inputs; };
-        };
+    nixosConfigurations = {
+      snafu-nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+           # Import the main module
+          ./configuration.nix
+          nix-flatpak.nixosModules.nix-flatpak
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # Move conflicting files out of the way instead of crashing home-manager
+            home-manager.backupFileExtension = "hmback";
+            home-manager.extraSpecialArgs.flake-inputs = inputs;
+            home-manager.users.mike.home.stateVersion = "25.05";
+            # Use Home Manager to manage Flatpaks
+            home-manager.users."mike".imports = [
+              # Add sops-nix support for home-manager
+              sops-nix.homeManagerModules.sops
+              # Flatpak NixOS configuration
+              nix-flatpak.homeManagerModules.nix-flatpak
+              ./users/mike/software/flatpak.nix
+              # Main home-manager configuration
+              ./users/mike/home.nix
+            ];
+          }
+        ];
+        specialArgs = { inherit inputs; };
       };
     };
+  };
 }
