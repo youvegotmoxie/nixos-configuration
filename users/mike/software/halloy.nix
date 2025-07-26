6af67fbd8d6440c717f16ba230afc068056e5a90
MikeB <youvegotmoxie@gmail.com>
2025-07-25 17:21:47 -0800

    [nixos-ansible] commit from automation

--- users/mike/software/halloy.nix
+++ users/mike/software/halloy.nix
@@ -2,14 +2,5 @@
   # Install and configure Halloy
   programs.halloy = {
     enable = true;
-    settings = {
-      "servers.liberachat" = {
-        channels = [
-          "#halloy"
-        ];
-        nickname = "moxie";
-        server = "irc.libera.chat";
-      };
-    };
   };
 }
